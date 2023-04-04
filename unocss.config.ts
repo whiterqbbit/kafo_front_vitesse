b8cf32a9994f9f79713bb19f6110be1e2201e9d9
whiterqbbit <wedd13@gmail.com>
2023-04-04 16:23:52 +0200

    fix: updated colors

--- unocss.config.ts
+++ unocss.config.ts
@@ -14,6 +14,43 @@ export default defineConfig({
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
   ],
+  theme: {
+    colors: {
+      cafe: {
+        900: '#1f1612',
+        800: '#261b17',
+        700: '#3b2a25',
+        600: '#503b34',
+        500: '#815a4f',
+        400: '#e3ba9f',
+        300: '#f5dac8',
+        200: '#fdeee2',
+        100: '#fff7f2',
+      },
+      grass: {
+        900: '#2e4744',
+        700: '#2a725f',
+        500: '#4ccb9f',
+        300: '#93e3b4',
+        100: '#d2ffe4',
+      },
+      blood: {
+        700: '#b32d5a',
+        500: '#e23d71',
+        300: '#ed6c8e',
+      },
+      eggplant: {
+        700: '#7f34d2',
+        500: '#a445ee',
+        300: '#ba70f4',
+      },
+      roast: {
+        600: '#ad4a33',
+        500: '#cc5f3e',
+        400: '#e37859',
+      },
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
