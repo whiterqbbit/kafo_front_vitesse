0014171668dcba0c85a341d8266bd2e65a4d65d8
whiterqbbit <wedd13@gmail.com>
2023-04-04 19:16:12 +0200

    testing

--- src/components/TheInput.vue
+++ src/components/TheInput.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-const { modelValue } = defineModel<{
-  modelValue: string
-}>()
-</script>
-
 <template>
   <input
     id="input"
-    v-model="modelValue"
     type="text"
     v-bind="$attrs"
     p="x-4 y-2"
