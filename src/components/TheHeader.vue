c83798324f4831ffc920ff995fae4754df5b4d4d
whiterqbbit <wedd13@gmail.com>
2023-04-06 16:16:40 +0200

    feat: header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,8 @@
 <template>
   <header h-35px w-full flex flex-row place-items-center justify-between bg-cafe-500 md:h-50px>
-    <div>
-      <img src="/src/assets/img/logo/kafo_logo_light" alt="logo" h-24>
-      <div w-35px bg-eggplant-300 md:h-50px md:w-50px>
-        LOGO
-      </div>
-    </div>
+    <RouterLink icon-btn to="/" title="home">
+      <img src="src/assets/img/logo/kafo_logo_light.png" alt="logo" class="h-9 pl-4 md:h-12">
+    </RouterLink>
     <div bg-cafe-600 font-title text-white>
       MENUS
     </div>
