227e0e661628fb1ae3c228775566f259ddc4c695
whiterqbbit <wedd13@gmail.com>
2023-04-07 22:46:40 +0200

    feat: galerie dans liste desktop

--- src/components/DesktopList.vue
+++ src/components/DesktopList.vue
@@ -1,30 +1,37 @@
 <template>
   <div>
     <div v-for="(coffee, index) in use_coffee_store().coffee_db" :key="index">
-      <div m-4 h-48 rounded-xl bg-cafe-300>
+      <div class="m-4 h-48 rounded-xl bg-cafe-300">
         {{ coffee.nom }}
-        <!-- <Galleria
-          :value="images" :num-visible="5" :circular="true" container-style="max-width: 640px"
+
+        <Galleria
+          class="w-240px"
+          :value="coffee.aws_miniatures"
+          :num-visible="3"
+          :circular="true"
+          container-style="max-width: 640px; max-height: 240px;"
           :show-item-navigators="true"
         >
           <template #item="slotProps">
-            <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block;">
+            <img
+              :src="slotProps.item.url"
+              :alt="coffee.nom"
+              style="width: 100%; height: 240px; display: block; object-fit: cover;"
+            >
           </template>
           <template #thumbnail="slotProps">
-            <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" style="display: block;">
+            <img
+              :src="slotProps.item.url"
+              :alt="coffee.nom"
+              style="width: 100%; height: 60px; display: block; object-fit: cover;"
+            >
           </template>
-        </Galleria> -->
-        <Image preview src="https://primefaces.org/cdn/primevue/images/galleria/galleria7.jpg" alt="Image" width="250" />
+        </Galleria>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Image from 'primevue/image'
-const images = [
-  'https://primefaces.org/cdn/primevue/images/galleria/galleria7.jpg',
-  'https://primefaces.org/cdn/primevue/images/galleria/galleria8.jpg',
-  'https://primefaces.org/cdn/primevue/images/galleria/galleria9.jpg',
-]
+import Galleria from 'primevue/galleria'
 </script>
