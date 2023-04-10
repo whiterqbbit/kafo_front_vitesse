466ee5c0b716f78a28ad7df745a03f85f130318f
whiterqbbit <wedd13@gmail.com>
2023-04-10 15:44:46 +0200

    feat: petite tete

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -14,15 +14,23 @@
         About
       </RouterLink>
     </nav>
-    <div btn @click="toggle_login">
+    <div v-if="!user.is_auth" btn @click="toggle_login">
       Login
+    </div>
+    <div v-else flex>
+      <img :src="user.pic_xsmall" alt="avatar" class="h-12 w-12 border border-cafe-100 rounded-full">
+      <button btn @click="user.logout">
+        Logout
+      </button>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
 import logo from '@/assets/img/logo/kafo_logo_light.png'
+import { use_user_store } from '@/stores/user'
 
+const user = use_user_store()
 function toggle_login() {
   display.login_modal = !display.login_modal
 }
