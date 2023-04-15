f572d75e30f2645510a0c6ffa33de0c9b6054ee4
jean-seb <sebastien.pingal@gmail.com>
2023-04-15 18:02:52 +0200

    feat: coffee page being build

--- src/pages/coffee/[id].vue
+++ src/pages/coffee/[id].vue
@@ -1,67 +1,126 @@
 <template>
-  <div class="container">
-    <div id="carousel_and_titles" class="flex flex-col bg-pink-500 p-2">
-      <div class="h-100 bg-red-500 text-center text-3xl">
+  <div class="flex flex-col gap-6 container">
+    <div id="carousel_and_titles" class="flex flex-col gap-6">
+      <div class="h-100 bg-red-500 text-left text-3xl">
         carousel
       </div>
-      <div id="title_and_Co" class="bg-teal-700 p-2 md:order-first">
-        <div id="title" class="bg-cafe-500">
-          Cafe machin
+
+      <div id="title_and_Co" class="px-5 md:order-first">
+        <div id="title" class="font-black text-3xl">
+          {{ selected_coffee?.nom }}
         </div>
-        <div id="personnes_et_minutes" class="flex flex-col bg-yellow-500 p-2 md:flex-row md:justify-between">
-          <div id="les_personnes" class="bg-cafe-100">
-            les personnes
+
+        <div
+          id="personnes_et_minutes"
+          class="flex flex-col font-bold md:flex-row md:justify-between"
+        >
+          <div v-if="selected_coffee?.attendance !== 0" id="les_personnes" class="">
+            {{ selected_coffee?.attendance }} personnes y sont en ce moment
           </div>
-          <div id="les_minutes" class="bg-cafe-500">
+          <div id="les_minutes" class="">
             les minutes
           </div>
         </div>
       </div>
     </div>
-    <div id="main" class="flex flex-col gap-9 bg-green-700 p-2 md:flex-row">
-      <div id="lefty" class="max-w-2xl bg-yellow-500 p-2">
-        <div id="description" class="bg-blue-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae
-          tincidunt nisl. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
-          Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
-          Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
-          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
-        </div>
-        <div id="tags" class="wrap flex flex-row flex-wrap gap-2 bg-green-500 p-2">
-          <div id="tag1" class="w-32 flex bg-cafe-500">
-            tag1
+
+    <div
+      v-if="!preferences.is_mobile"
+      id="tags"
+      class="wrap flex flex-row flex-wrap gap-2"
+    >
+      <div id="tag1" class="w-32 flex">
+        tag1
+      </div>
+      <div id="tag2" class="w-32 flex">
+        tag2
+      </div>
+      <div id="tag3" class="w-32 flex">
+        tag3
+      </div>
+      <div id="tag4" class="w-32 flex">
+        tag4
+      </div>
+      <div id="tag5" class="w-32 flex">
+        tag5
+      </div>
+    </div>
+
+    <div id="main" class="flex flex-col gap-6 px-5 md:flex-row">
+      <div id="lefty" class="max-w-2xl flex flex-col gap-6">
+        <div id="description" class="flex flex-col gap-6">
+          <div class="flex flex-col gap-2">
+            <div class="flex flex-row gap-2">
+              <div class="font-bold">
+                Ambiance :
+              </div>
+              <div> {{ selected_coffee?.tags.find(tag => ['Studieux', 'Calme', 'Animé'].includes(tag)) }}</div>
+            </div>
+
+            <div v-if="selected_coffee?.desc" class="flex flex-col gap-2">
+              <div class="font-bold">
+                A propos :
+              </div>
+              <div> {{ selected_coffee?.desc }}</div>
+            </div>
+
+            <div v-if="selected_coffee?.acces_wifi" class="flex flex-row gap-2">
+              <div class="font-bold">
+                Wifi :
+              </div>
+              <div> {{ selected_coffee?.acces_wifi }}</div>
+            </div>
           </div>
-          <div id="tag2" class="w-32 flex bg-cafe-500">
-            tag2
+
+          <div v-if="preferences.is_mobile" id="tags" class="wrap flex flex-col flex-wrap gap-2">
+            <div class="font-bold">
+              Service et spécificités :
+            </div>
+            <div class="flex flex-row flex-wrap gap-2">
+              <div id="tag1" class="w-32 flex">
+                tag1
+              </div>
+              <div id="tag2" class="w-32 flex">
+                tag2
+              </div>
+              <div id="tag3" class="w-32 flex">
+                tag3
+              </div>
+              <div id="tag4" class="w-32 flex">
+                tag4
+              </div>
+              <div id="tag5" class="w-32 flex">
+                tag5
+              </div>
+            </div>
           </div>
-          <div id="tag3" class="w-32 flex bg-cafe-500">
-            tag3
-          </div>
-          <div id="tag4" class="w-32 flex bg-cafe-500">
-            tag4
-          </div>
-          <div id="tag5" class="w-32 flex bg-cafe-500">
-            tag5
-          </div>
-        </div>
-        <div id="map_and_co" class="bg-teal-700 p-2">
-          <div id="map" class="bg-cafe-500">
-            map
-          </div>
-          <div id="co" class="bg-cafe-500">
-            co
+
+          <div v-if="selected_coffee?.adresse" id="map_and_co" class="flex flex-row place-items-center">
+            <div class="flex flex-col">
+              <div id="adresse" class="">
+                {{ selected_coffee?.adresse }}
+              </div>
+              <div id="selected_coffee?.metro" class="">
+                Métro : {{ selected_coffee?.metro }}
+              </div>
+            </div>
+            <div id="go" class="h-20 flex flex-row place-items-center rounded-3xl bg-grass-500 p-3 font-bold text-cafe-100">
+              <div>
+                Y aller !
+              </div>
+              <img src="https://img.icons8.com/ios/50/000000/forward.png">
+            </div>
           </div>
         </div>
       </div>
-      <div id="righty" class="bg-blue-500 p-2">
-        <div id="ingredients" class="bg-cafe-500">
-          ingredients
-        </div>
-        <div id="preparation" class="bg-cafe-500">
-          preparation
-        </div>
+    </div>
+
+    <div id="righty" class="">
+      <div id="ingredients" class="">
+        ingredients
+      </div>
+      <div id="preparation" class="">
+        preparation
       </div>
     </div>
   </div>
@@ -69,10 +128,10 @@
 
 <script setup lang="ts">
 const props = defineProps({
-  id: {
-    type: Number,
-    required: true,
-  },
+  id: Number,
 })
+const selected_coffee_id = Number(props.id)
 const coffee_store = use_coffee_store()
+coffee_store.selected_id = selected_coffee_id
+const selected_coffee = computed(() => coffee_store.selected)
 </script>
