1a847ec3eedf0012d2015ed7bb7b557f9ca85f8c
whiterqbbit <wedd13@gmail.com>
2023-04-22 10:03:15 +0200

    fix: color

--- src/components/DesktopHeader.vue
+++ src/components/DesktopHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header h-35px w-full flex flex-row place-items-center justify-between bg-cafe-500 md:h-50px>
-    <RouterLink class="icon-btn" to="/">
+  <header h-35px w-full flex flex-row place-items-center justify-between bg-cafe-600 md:h-50px>
+    <RouterLink class="icon-btn" to="/landing">
       <img :src="logo" alt="logo" class="h-9 pl-4 md:h-12">
     </RouterLink>
     <nav flex gap-5 font-black text-white>
@@ -36,7 +36,7 @@
 </template>
 
 <script setup lang="ts">
-import logo from '@/assets/img/logo/kafo_logo_light.png'
+import logo from '@/assets/img/logo/kafo_logo_white.png'
 import { use_user_store } from '@/stores/user'
 
 const user = use_user_store()
