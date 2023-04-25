479d3a9816916c2ca2792941ddb26e746c090e92
whiterqbbit <wedd13@gmail.com>
2023-04-25 10:19:32 +0200

    feat: legal stuff

--- src/pages/mentions-legales.vue
+++ src/pages/mentions-legales.vue
@@ -1,94 +1,80 @@
 <template>
   <LandingHeader />
-  <div>
-    CONDITIONS GÉNÉRALES D'UTILISATION
+  <div class="container">
+    <div class="mb-22 font-title">
+      CONDITIONS GÉNÉRALES D'UTILISATION
+    </div>
 
-    INFORMATIONS LÉGALES
+    <div class="title">
+      INFORMATIONS LÉGALES
+    </div>
     Le site Kafo (ci-après "le Site") est édité par la société SAS Kafo, dont le siège social est situé au 40 rue Alexandre Dumas, 75011 Paris. Vous pouvez nous contacter par e-mail à l'adresse vincent@kafo.work ou par téléphone au 0695174679.
 
-    OBJET
+    <div class="title">
+      OBJET
+    </div>
     Le Site est une plateforme de mise en relation entre des cafés/restaurants et des freelances, salariés ou étudiants qui souhaitent travailler ensemble dans des espaces de coworking temporaires. Les présentes conditions générales d'utilisation (CGU) ont pour objet de définir les droits et obligations des parties dans le cadre de l'utilisation du Site.
 
-    ACCEPTATION DES CGU
+    <div class="title">
+      ACCEPTATION DES CGU
+    </div>
     L'utilisation du Site implique l'acceptation pleine et entière des présentes CGU. Si vous n'acceptez pas ces conditions, nous vous demandons de ne pas utiliser le Site.
 
-    CONDITIONS D'ACCÈS
+    <div class="title">
+      CONDITIONS D'ACCÈS
+    </div>
     Pour accéder au Site, les utilisateurs doivent être âgés d'au moins 18 ans et ne pas être soumis à des restrictions géographiques.
 
-    DONNÉES PERSONNELLES
+    <div class="title">
+      DONNÉES PERSONNELLES
+    </div>
     Les utilisateurs fournissent leurs données personnelles pour permettre leur mise en relation avec d'autres membres de la communauté Kafo. Les utilisateurs peuvent demander la suppression de leur compte en envoyant un e-mail à l'adresse indiquée à l'article 1. Pour plus d'informations sur la manière dont nous traitons vos données personnelles, veuillez consulter notre politique de confidentialité.
 
-    PROPRIÉTÉ INTELLECTUELLE
+    <div class="title">
+      PROPRIÉTÉ INTELLECTUELLE
+    </div>
     Tous les éléments du Site, notamment les marques, logos, textes, images et autres éléments de propriété intellectuelle, sont la propriété exclusive de la société SAS Kafo, sauf mention contraire. Toute reproduction, modification ou utilisation non autorisée de ces éléments est strictement interdite.
 
-    RESPONSABILITÉ
+    <div class="title">
+      RESPONSABILITÉ
+    </div>
     SAS Kafo met tout en œuvre pour assurer la disponibilité et le bon fonctionnement du Site, mais ne peut garantir que celui-ci sera toujours accessible ou exempt d'erreurs. En conséquence, SAS Kafo ne pourra être tenu responsable de tout dommage direct ou indirect résultant de l'utilisation du Site.
 
-    RÉSOLUTION DES LITIGES ET JURIDICTION APPLICABLE
+    <div class="title">
+      RÉSOLUTION DES LITIGES ET JURIDICTION APPLICABLE
+    </div>
     Tout litige relatif à l'utilisation du Site sera soumis à la loi française et aux tribunaux compétents de Paris.
 
-    MODIFICATION DES CGU
+    <div class="title">
+      MODIFICATION DES CGU
+    </div>
     SAS Kafo se réserve le droit de modifier les présentes CGU à tout moment. Les utilisateurs seront informés de ces modifications par une notification sur le Site et devront accepter les nouvelles conditions pour continuer à utiliser le Site.
 
-    SUSPENSION OU SUPPRESSION DES COMPTES
+    <div class="title">
+      SUSPENSION OU SUPPRESSION DES COMPTES
+    </div>
     SAS Kafo se réserve le droit de suspendre ou de supprimer les comptes des utilisateurs qui ne respectent pas les présentes conditions générales d'utilisation, ou qui ont fourni de fausses informations lors de leur inscription. Les utilisateurs concernés seront informés de cette suspension ou suppression par e-mail et ne pourront plus accéder au Site.
 
-    LIENS VERS D'AUTRES SITES
+    <div class="title">
+      LIENS VERS D'AUTRES SITES
+    </div>
     Le Site peut contenir des liens vers des sites tiers. Ces liens sont fournis à titre informatif et SAS Kafo n'est pas responsable du contenu ou des politiques de ces sites. Les utilisateurs sont encouragés à consulter les conditions d'utilisation et les politiques de confidentialité des sites tiers avant de les utiliser.
 
-    DROIT APPLICABLE ET JURIDICTION
+    <div class="title">
+      DROIT APPLICABLE ET JURIDICTION
+    </div>
     Les présentes conditions générales d'utilisation sont régies par le droit français. En cas de litige relatif à l'interprétation, la validité ou l'exécution des présentes CGU, les tribunaux de Paris, France, seront compétents pour trancher le litige.
 
-    DISPOSITIONS GÉNÉRALES
+    <div class="title">
+      DISPOSITIONS GÉNÉRALES
+    </div>
     Si une disposition des présentes CGU est jugée invalide, illégale ou inapplicable par un tribunal compétent, les autres dispositions resteront en vigueur et de plein effet. Le fait pour SAS Kafo de ne pas exercer ou de ne pas faire valoir un droit ou une disposition des présentes CGU ne constitue pas une renonciation à ce droit ou à cette disposition.
-    POLITIQUE DE CONFIDENTIALITÉ
-
-    INTRODUCTION
-    La société SAS Kafo (ci-après "Kafo" ou "nous") attache une grande importance à la protection de la vie privée et des données personnelles de ses utilisateurs (ci-après "vous" ou "l'utilisateur"). La présente politique de confidentialité a pour objet de vous informer sur la manière dont nous collectons, utilisons et protégeons vos données personnelles lorsque vous utilisez notre site internet (ci-après "le Site").
-
-    COLLECTE DES DONNÉES PERSONNELLES
-    Nous collectons les données personnelles que vous nous fournissez lorsque vous créez un compte sur le Site, telles que votre nom, prénom, adresse e-mail, numéro de téléphone et toute autre information que vous choisissez de nous communiquer.
-
-    UTILISATION DES DONNÉES PERSONNELLES
-    Vos données personnelles sont utilisées pour les finalités suivantes :
-
-    Vous permettre d'accéder et d'utiliser le Site et ses services
-    Vous mettre en relation avec d'autres membres de la communauté Kafo
-    Vous informer des actualités, mises à jour et offres promotionnelles liées à Kafo
-    Améliorer la qualité et la pertinence de notre Site et de nos services
-    PARTAGE DES DONNÉES PERSONNELLES
-    Nous ne partageons vos données personnelles qu'avec des tiers dans les cas suivants :
-
-    Avec votre consentement : Nous pouvons partager vos données avec d'autres membres de la communauté Kafo pour faciliter la mise en relation.
-    Pour respecter une obligation légale : Nous pourrions être amenés à divulguer vos données si la loi nous y oblige ou pour protéger nos droits, notre propriété ou notre sécurité.
-    SÉCURITÉ DES DONNÉES PERSONNELLES
-    Nous mettons en œuvre des mesures techniques et organisationnelles appropriées pour protéger vos données personnelles contre la perte, l'accès non autorisé ou la divulgation. Cependant, nous ne pouvons garantir la sécurité absolue des informations transmises sur Internet.
-
-    DURÉE DE CONSERVATION DES DONNÉES PERSONNELLES
-    Nous conservons vos données personnelles aussi longtemps que nécessaire pour les finalités décrites dans la présente politique de confidentialité, ou pour respecter nos obligations légales.
-
-    DROITS DES UTILISATEURS
-    Vous disposez des droits suivants concernant vos données personnelles :
-
-    Droit d'accès : Vous pouvez demander à accéder à vos données personnelles.
-    Droit de rectification : Vous pouvez demander la rectification de vos données personnelles si elles sont inexactes.
-    Droit à l'effacement : Vous pouvez demander la suppression de vos données personnelles.
-    Droit à la limitation du traitement : Vous pouvez demander la limitation du traitement de vos données personnelles dans certaines circonstances.
-    Droit d'opposition : Vous pouvez vous opposer au traitement de vos données personnelles.
-
-    Pour exercer ces droits, veuillez nous contacter à l'adresse e-mail indiquée à la fin de cette politique de confidentialité. Nous nous engageons à répondre à votre demande dans un délai d'un mois. Dans certains cas, nous pourrions être amenés à prolonger ce délai, auquel cas nous vous en informerons.
-
-    COOKIES
-    Le Site utilise des cookies pour améliorer votre expérience utilisateur et collecter des informations sur l'utilisation du Site. Les cookies sont de petits fichiers texte stockés sur votre ordinateur ou votre appareil mobile lorsque vous visitez un site. Vous pouvez gérer et supprimer les cookies en modifiant les paramètres de votre navigateur.
-
-    TRANSFERTS INTERNATIONAUX DE DONNÉES
-    Nous nous efforçons de stocker et de traiter vos données personnelles dans l'Union européenne. Cependant, il se peut que nous transférions vos données à des prestataires situés en dehors de l'Union européenne. Dans ce cas, nous nous assurons que ces transferts sont effectués conformément à la législation en vigueur et offrent un niveau de protection adéquat.
-
-    MODIFICATIONS DE LA POLITIQUE DE CONFIDENTIALITÉ
-    Nous nous réservons le droit de modifier la présente politique de confidentialité à tout moment. Les modifications apportées à la politique de confidentialité seront publiées sur le Site, et nous vous recommandons de les consulter régulièrement.
-
-    CONTACT
-    Si vous avez des questions ou des préoccupations concernant la présente politique de confidentialité ou le traitement de vos données personnelles, veuillez nous contacter par e-mail à l'adresse suivante : vincent@kafo.work.
   </div>
   <LandingFooter class="mt-20 h-48" />
 </template>
+
+<style>
+.title {
+  @apply text-xl font-bold mb-1 mt-6;
+}
+</style>
