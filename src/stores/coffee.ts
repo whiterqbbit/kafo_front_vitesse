4353e99ac823060e1f57183a749f863229109143
jean-seb <sebastien.pingal@gmail.com>
2023-04-25 10:50:13 +0200

    feat: filters

--- src/stores/coffee.ts
+++ src/stores/coffee.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import type { Cafe } from './xano.d'
+import type { Cafe, CafeTag } from './xano.d'
 
 const xano_url = `${import.meta.env.VITE_XANO_API_URL}/api:EW8LvnML/coffee`
 
@@ -10,6 +10,38 @@ export const use_coffee_store = defineStore('coffee', () => {
   const selected_id = ref<number | null>(null)
 
   const selected = computed(() => db.value?.find(cafe => cafe.id === selected_id.value) ?? null)
+  const db_filtered = computed(() => {
+    const array_price: CafeTag[] = []
+    const array_noise: CafeTag[] = []
+    const array_other: CafeTag[] = []
+    const filtered_coffee_array: Cafe[] = []
+
+    if (filters.pricing_free) array_price.push('Gratuit')
+    if (filters.pricing_coffee) array_price.push('Café', 'Restaurant', 'Bar', 'Brasserie', 'Tiers lieu', 'Autre lieu')
+    if (filters.pricing_hourly) array_price.push('Coworking')
+
+    if (filters.noise_level_silent) array_noise.push('Studieux')
+    if (filters.noise_level_calm) array_noise.push('Calme')
+    if (filters.noise_level_lively) array_noise.push('Animé')
+
+    if (filters.wifi) array_other.push('Wifi')
+    if (filters.power) array_other.push('Prises')
+    if (filters.our_picks) array_other.push('Top rated')
+    // gérer non vides
+
+    if (db.value) {
+      for (const cafe of db.value) {
+        const { tags, is_open } = cafe
+        const price_matched = !array_price.length || array_price.some(r => tags.includes(r))
+        const noise_matched = !array_noise.length || array_noise.some(r => tags.includes(r))
+        const other_matched = !array_other.length || array_other.every(r => tags.includes(r))
+        const open_matched = !filters.open_now || is_open
+
+        if (price_matched && noise_matched && other_matched && open_matched) filtered_coffee_array.push(cafe)
+      }
+    }
+    return filtered_coffee_array
+  })
 
   async function fetch_db() {
     db.value = null
@@ -35,5 +67,5 @@ export const use_coffee_store = defineStore('coffee', () => {
     }
   }
 
-  return { fetch_db, db, db_loading, db_error, selected, selected_id }
+  return { fetch_db, db, db_loading, db_filtered, db_error, selected, selected_id }
 })
