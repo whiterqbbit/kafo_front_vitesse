db0f57a6c2cb7dda8258ed98a67cf9a6998f4766
jean-seb <sebastien.pingal@gmail.com>
2023-04-29 15:33:40 +0200

    feat: btn-green and btn-green-full

--- unocss.config.ts
+++ unocss.config.ts
@@ -11,6 +11,8 @@ import {
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 w-fit rounded-xl inline-block place-content-center cursor-pointer transition duration-100 hover:btn-scale disabled:(cursor-default bg-gray-600 opacity-50)'],
+    ['btn-green', 'btn bg-grass-500 text-cafe-50 hover:bg-grass-600'],
+    ['btn-green-full', 'btn-green w-full'],
     ['btn-scale', 'scale-103 -translate-y-0.3 duration-60'],
     ['btn-light', 'px-4 cursor-pointer py-1 h-fit w-fit place-self-center rounded-xl font-normal text-base text-gray-400 transition hover:(rounded-xl text-cafe-700 bg-opacity-25 bg-stone-200)'],
     ['font-title', 'text-3xl font-bold text-center text-cafe-700'],
