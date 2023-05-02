fb3959922f4c0e593fd27f18f6b039b8f546a118
whiterqbbit <wedd13@gmail.com>
2023-05-02 11:53:29 +0200

    feat: burger menu

--- src/components/DesktopHeader.vue
+++ src/components/DesktopHeader.vue
@@ -24,14 +24,20 @@
         A Propos
       </RouterLink>
     </nav>
-    <div v-if="!user.is_auth" class="m-2 btn-grass" @click="display.login_modal = !display.login_modal">
-      Login
+    <div v-if="!user.is_auth" class="m-2 font-bold btn-grass" @click="display.login_modal = !display.login_modal">
+      Se connecter
     </div>
-    <div v-else flex>
-      <img :src="user.pic_xsmall ? user.pic_xsmall : default_user_pic" alt="avatar" class="h-12 w-12 border border-cafe-100 rounded-full">
-      <button class="m-2 btn-grass" @click="user.logout">
-        Logout
-      </button>
+    <img v-else :src="user.pic_xsmall ? user.pic_xsmall : default_user_pic" alt="avatar" class="h-12 w-12 border border-cafe-100 rounded-full" @click="display.burger_menu = !display.burger_menu">
+    <div v-if="display.burger_menu" class="absolute right-0 top-[50px] flex flex-col rounded-bl-xl bg-cafe-600 p-4 font-semibold text-white">
+      <div class="menu-item">
+        Mon profil
+      </div>
+      <div class="menu-item">
+        Placeholder
+      </div>
+      <div class="menu-item" @click="logout">
+        Se déconnecter
+      </div>
     </div>
   </header>
 </template>
@@ -39,7 +45,18 @@
 <script setup lang="ts">
 import logo from '@/assets/img/logo/kafo_logo_white.png'
 import { use_user_store } from '@/stores/user'
-import default_user_pic from '@/assets/img/Profil3.png'
+import default_user_pic from '@/assets/img/default_user_pic.png'
 
 const user = use_user_store()
+
+function logout() {
+  user.logout()
+  display.burger_menu = false
+}
 </script>
+
+<style scoped>
+.menu-item{
+  @apply cursor-pointer
+}
+</style>
