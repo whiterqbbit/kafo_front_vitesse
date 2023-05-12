18626271329255ce48141f3376abe1e2cc87015d
sebastien <sebastien.pingal@gmail.com>
2023-05-12 18:22:10 +0200

    feat: toggle_selected_club

--- src/components/ClubList.vue
+++ src/components/ClubList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-if="selected_clubs">
+  <div v-if="selected_clubs" class="flex flex-wrap gap-2">
     <div 
       v-for="club in selected_clubs" 
-      :key="club.id || 'fallback'"
-      class="rounded-full bg-grass-500 px-2 py-0.5 text-sm text-white w-fit"
-      @click="add_selected_club(club)"
+      :key="club.uuid || 'fallback'"
+      class="rounded-full bg-grass-500 px-2 py-0.5 text-sm text-white w-fit cursor-pointer"
+      @click="toggle_selected_club(club)"
     >
       {{club.nom}}
     </div>
@@ -12,9 +12,9 @@
   <div class="flex flex-wrap justify-between gap-2 px-1 py-2 max-h-30 overflow-auto border border-1 border-cafe-400 rounded-lg">
     <div
       v-for="club in filtered_clubs"
-      :key="club.id || 'fallback'"
-      class="rounded-full bg-cafe-500 px-2 py-0.5 text-sm text-white"
-      @click="add_selected_club(club)"
+      :key="club.uuid || 'fallback'"
+      class="rounded-full bg-cafe-500 px-2 py-0.5 text-sm text-white cursor-pointer"
+      @click="toggle_selected_club(club)"
     >
       {{ club.nom }}
     </div>
@@ -44,24 +44,35 @@ watch(
 )
 
 const filtered_clubs = computed(() => {
-  const excludeSelected = (club) => !selected_clubs.value.some(selected => selected.id === club.id)
+  // Define a function called "excludeSelected" that takes a club as an argument
+  // and returns true if the club is NOT in the "selected_clubs" list.
+  const is_not_selected = (club) => !selected_clubs.value.some(selected => selected.uuid === club.uuid)
 
-  const available_clubs = clubs.value.filter(excludeSelected)
+  // Create a new array called "available_clubs" by filtering out the clubs
+  // in the "clubs" list that are in the "selected_clubs" list.
+  const available_clubs = clubs.value.filter(club => is_not_selected(club))
+  console.log('available_clubs', available_clubs)
 
+  // If there's a search input, return the filtered clubs based on the search results.
   if (props.search_input) {
+    // Get the search results and extract only the club items.
     const filteredResults = searchResults.value?.map(result => result.item)
-    return filteredResults.filter(excludeSelected)
+
+    // Return the filtered clubs, excluding the clubs in the "selected_clubs" list.
+    return filteredResults.filter(club => is_not_selected(club))
   } else {
+    // If there's no search input, return the available clubs
+    // that are not in the "selected_clubs" list.
     return available_clubs;
   }
 })
 
-function add_selected_club(club: Club) {
-  console.log('add_selected_club')
-  if(!selected_clubs.value.some(selected_club => selected_club.id === club.id)) {
-    selected_clubs.value.push(club)
+function toggle_selected_club(club: Club) {
+  console.log('toggle_selected_club')
+  if (!selected_clubs.value.some(selected_club => selected_club.uuid === club.uuid)) {
+    selected_clubs.value = [...selected_clubs.value, club]
   } else {
-    selected_clubs.value = selected_clubs.value.filter(selected_club => selected_club.id !== club.id)
+    selected_clubs.value = selected_clubs.value.filter(selected_club => selected_club.uuid !== club.uuid)
   }
 }
 </script>
