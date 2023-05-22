91cc5d6005cb5d1ceec1175ce8e3ef326529cf77
sebastien <sebastien.pingal@gmail.com>
2023-05-22 18:42:23 +0200

    feat: calendar link

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="w-full flex rounded-xl bg-cafe-400 p-1">
     <ADialog
+      v-if="confirmation_modal.session"
       v-model="confirmation_modal.display"
       :title="confirmation_modal.is_joining ? 'Vous avez rejoint la session !' : 'Vous avez quitté la session !'"
-      :subtitle="`${get_relative_date_from_date(day)} de ${confirmation_modal.start} à ${confirmation_modal.end}`"
+      :subtitle="`${get_relative_date_from_date(day)} de ${convert_to_hour_format(confirmation_modal?.session?.start)} à ${convert_to_hour_format(confirmation_modal.session.end)}`"
       :text="confirmation_modal.is_joining ? 'Wahou trop bien' : 'trop nul'"
     >
       <div class="m-4 flex flex-col gap-4">
@@ -17,7 +18,7 @@
             Retour
           </ABtn>
           <ABtn variant="light" icon="i-bx-calendar">
-            Google Calendar
+            <a :href="confirmation_modal.calendar_link" target="_blank">Google Calendar</a>
           </ABtn>
         </div>
       </div>
@@ -32,21 +33,21 @@
     </div>
     <div v-if="events_of_the_day" class="w-3/4 flex flex-col gap-1">
       <div
-        v-for="event in events_of_the_day"
-        :key="event.id || 'Fallback'"
+        v-for="session in events_of_the_day"
+        :key="session.id || 'Fallback'"
         class="w-full flex flex-col gap-1 rounded-xl bg-cafe-100 p-3"
       >
-        <div v-if="event.in_current_slot" class="text-xl font-bold uppercase">
+        <div v-if="session.in_current_slot" class="text-xl font-bold uppercase">
           En ce moment
         </div>
         <div>
-          {{ convert_to_hour_format(event.start) }} - {{ convert_to_hour_format(event.end) }}
+          {{ convert_to_hour_format(session.start) }} - {{ convert_to_hour_format(session.end) }}
         </div>
-        <div v-if="event.user_id?.length && !event.in_current_slot" class="my-2 mt-5 max-h-80 flex flex-col gap-4 overflow-auto">
-          <AvatarStack :attendees="event.user_id" />
+        <div v-if="session.user_id?.length && !session.in_current_slot" class="my-2 mt-5 max-h-80 flex flex-col gap-4 overflow-auto">
+          <AvatarStack :attendees="session.user_id" />
         </div>
-        <div v-else-if="event.user_id?.length">
-          <div v-for="user in event.user_id" :key="user.id || 'fallback-key'" class="flex items-center gap-3 rounded-xl p-1.5" :class="user_store.id === user.id ? 'bg-cafe-300' : '' ">
+        <div v-else-if="session.user_id?.length">
+          <div v-for="user in session.user_id" :key="user.id || 'fallback-key'" class="flex items-center gap-3 rounded-xl p-1.5" :class="user_store.id === user.id ? 'bg-cafe-300' : '' ">
             <img :src="user.pic_xsmall ? user.pic_xsmall : default_user_pic" class="aspect-square h-15 rounded-full object-cover">
             <div>
               <div class="text-lg font-bold">
@@ -56,9 +57,9 @@
             </div>
           </div>
         </div>
-        <button class="w-full p-2 font-bold btn-grass" @click="submit_to_event(event)">
-          <div v-if="!is_loading.get(event.id)">
-            {{ event_store?.is_user_in_event(event.id) ? 'Quitter' : 'Rejoindre' }}
+        <button class="w-full p-2 font-bold btn-grass" @click="submit_to_event(session)">
+          <div v-if="!is_loading.get(session.id)">
+            {{ event_store?.is_user_in_event(session.id) ? 'Quitter' : 'Rejoindre' }}
           </div>
           <ThreeDotsSpinner v-else class="mx-auto" />
         </button>
@@ -75,7 +76,7 @@
 <script setup lang="ts">
 import { get_day_from_date, get_month_name_from_date, get_relative_date_from_date, is_slot_current } from '@/utils/date_utils'
 import default_user_pic from '@/assets/img/default_user_pic.png'
-import type { Event } from '@/stores/xano'
+import type { Session } from '@/stores/xano'
 
 const props = defineProps({
   day: {
@@ -87,38 +88,38 @@ const props = defineProps({
 
 const event_store = use_event_store()
 const user_store = use_user_store()
+const place_store = use_place_store()
 
 const day = ref(props.day ?? new Date())
 const is_loading = ref(new Map())
 const confirmation_modal = reactive({
-  id: 0,
+  session: null as Session | null,
   is_joining: false,
   display: false,
-  start: '',
-  end: '',
+  calendar_link: '',
 })
 
-// Creates an array of the event of the day and add useful variables to each event
+// Creates an array of the session of the day and add useful variables to each session
 const events_of_the_day = computed(() => {
   if (!event_store.selected_place_events) {
     return null
   }
 
-  let events = event_store.selected_place_events?.filter((event) => {
-    if (!event.jour) return false
-    const event_date = new Date(event.jour)
+  let events = event_store.selected_place_events?.filter((session) => {
+    if (!session.jour) return false
+    const event_date = new Date(session.jour)
     if (
       event_date.getDate() === day.value.getDate()
       && event_date.getMonth() === day.value.getMonth()
       && event_date.getFullYear() === day.value.getFullYear()
     ) {
-      event.in_current_slot = is_slot_current(event.start, event.end)
+      session.in_current_slot = is_slot_current(session.start, session.end)
 
       let date_relative_to_today = get_relative_date_from_date(day.value)
-      if ((date_relative_to_today === 'Aujourd\'hui') && event.in_current_slot) {
+      if ((date_relative_to_today === 'Aujourd\'hui') && session.in_current_slot) {
         date_relative_to_today = 'En ce moment'
       }
-      event.relative_to_now = date_relative_to_today
+      session.relative_to_now = date_relative_to_today
       return true
     }
     return false
@@ -135,25 +136,46 @@ function convert_to_hour_format(date_to_compute: Date) {
 
   return formattedTime
 }
-async function submit_to_event(event: Event) {
+async function submit_to_event(session: Session) {
   if (!user_store.id) {
     display.login_modal = true
     return false
   }
-  is_loading.value.set(event.id, true)
+  is_loading.value.set(session.id, true)
   try {
-    const res = await event_store.submit_events(event.id)
+    const res = await event_store.submit_events(session.id)
     if (res.ok) {
-      confirmation_modal.id = event.id
+      confirmation_modal.session = session
       confirmation_modal.is_joining = res.subscribe
       confirmation_modal.display = true
-      confirmation_modal.start = convert_to_hour_format(event.start)
-      confirmation_modal.end = convert_to_hour_format(event.end)
+      const converted_start = convertDate(session.start)
+      const converted_end = convertDate(session.end)
+      const place_name = place_store?.db?.find(place => place.id === session.coffee_id)?.nom
+      const place_adresse = place_store?.db?.find(place => place.id === session.coffee_id)?.adresse
+      confirmation_modal.calendar_link = `https://calendar.google.com/calendar/u/0/r/eventedit?dates=${converted_start}/${converted_end}&text=${place_name}&location=${place_adresse}`
     }
   } catch (error) {
     console.error(error)
   } finally {
-    is_loading.value.set(event.id, false)
+    is_loading.value.set(session.id, false)
   }
+}
+
+function convertDate(unixTimestamp: Date) {
+  // Create a new date object from the Unix timestamp
+  const dateObj = new Date(unixTimestamp)
+
+  // Get the year, month, day, hour, minute, and second components of the date
+  const year = dateObj.getUTCFullYear()
+  const month = (dateObj.getUTCMonth() + 1).toString().padStart(2, '0')
+  const day = dateObj.getUTCDate().toString().padStart(2, '0')
+  const hour = dateObj.getUTCHours().toString().padStart(2, '0')
+  const minute = dateObj.getUTCMinutes().toString().padStart(2, '0')
+  const second = dateObj.getUTCSeconds().toString().padStart(2, '0')
+
+  // Combine the components into the desired format
+  const formattedDate = `${year}${month}${day}T${hour}${minute}${second}Z`
+
+  return formattedDate
 }
 </script>
