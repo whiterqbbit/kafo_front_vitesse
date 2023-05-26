c68e2181646c42ee51bed31282cdf53a5c04f01a
whiterqbbit <wedd13@gmail.com>
2023-05-26 21:44:10 +0200

    fix: invisible button

--- unocss.config.ts
+++ unocss.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     ['btn', 'px-4 py-1 w-fit rounded-xl inline-block place-content-center cursor-pointer transition duration-100 hover:btn-scale disabled:(cursor-default bg-gray-600 opacity-50)'],
     ['btn-scale', 'scale-103 -translate-y-0.3 duration-60'],
     ['btn-grass', 'btn bg-grass-500 text-cafe-50 hover:bg-grass-600'],
-    ['btn-cafe', 'btn bg-cafe-600 text-cafe-50 hover:bg-cafe-700'],
+    ['btn-cafe', 'btn bg-cafe-600 text-cafe-500 hover:bg-cafe-700 hover:text-cafe-50'],
     ['btn-cafe-light', 'btn text-cafe-600 bg-cafe-50 hover:shadow-sm border border-cafe-400 hover:border-cafe-500'],
     ['btn-grass-full', 'btn-grass w-full'],
     ['btn-light', 'px-4 cursor-pointer py-1 h-fit w-fit place-self-center rounded-xl font-normal text-base text-gray-400 transition hover:(rounded-xl text-cafe-700 bg-white)'],
