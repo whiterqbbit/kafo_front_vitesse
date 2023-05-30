280b619125a884dc3487887430eac68b10d7633c
whiterqbbit <wedd13@gmail.com>
2023-05-30 17:25:42 +0200

    feat: sorting by our_fav

--- test/stores/place.test.ts
+++ test/stores/place.test.ts
@@ -33,14 +33,24 @@ describe('place', () => {
     }
   })
 
-  // it('sorts the db by our_fav', () => {
-  //   if (place_store.db) {
-  //     const not_favorite_place = place_store.db.find(p => !p.our_fav)
-  //     second_place.our_fav = true
-  //     place_store.sort_places()
-  //     expect(place_store.db[0].id).toBe(second_place.id)
-  //   } else {
-  //     throw new Error('db is null')
-  //   }
-  // })
+  it('sorts the db by our_fav', () => {
+    if (place_store.db) {
+      let not_favorite_place_index = -1
+      let not_favorite_place_id = -1
+      for (let i = place_store.db.length - 1; i >= 0; i--) {
+        if (!place_store.db[i].our_fav) {
+          not_favorite_place_index = i
+          not_favorite_place_id = place_store.db[i].id
+          break
+        }
+      }
+      if (not_favorite_place_index === -1) throw new Error('no place has our_fav = false')
+      place_store.db[not_favorite_place_index].our_fav = true
+      place_store.sort_places()
+      const new_index = place_store.db.findIndex(p => p.id === not_favorite_place_id)
+      expect(new_index).toBeLessThan(not_favorite_place_index)
+    } else {
+      throw new Error('db is null')
+    }
+  })
 })
