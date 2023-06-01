1bbcd359fb5e1b0bfe18ea18be7ce93156c9295c
sebastien <sebastien.pingal@gmail.com>
2023-06-01 10:27:29 +0200

    feat: starting to e2e test

--- cypress/e2e/basic.spec.ts
+++ cypress/e2e/basic.spec.ts
@@ -3,12 +3,21 @@ context('Basic', () => {
     cy.visit('/')
   })
 
-  it('basic nav', () => {
+  it('displays landing', () => {
     cy.url()
       .should('eq', 'http://localhost:3333/')
 
-    // cy.contains('[Home Layout]')
-    //   .should('exist')
+    cy.contains('Coworke dans des')
+    cy.contains('Coworkez Maintenant')
+    cy.get('header').contains('A Propos')
+    cy.contains('Contact')
+    cy.contains('Mentions légales')
+    cy.contains('Confidentialité')
+    cy.contains('Nos valeurs')
+    cy.contains('Devenir partenaire')
+    cy.contains('Instagram')
+    cy.contains('Linkedin')
+    cy.get('footer').contains('A Propos')
 
     // cy.get('#input')
     //   .type('Vitesse{Enter}')
@@ -17,20 +26,35 @@ context('Basic', () => {
 
     // cy.contains('[Default Layout]')
     //   .should('exist')
-
-    // cy.get('[btn]')
-    //   .click()
-    //   .url()
-    //   .should('eq', 'http://localhost:3333/')
   })
 
-  // it('markdown', () => {
-  //   cy.get('[title="About"]')
-  //     .click()
-  //     .url()
-  //     .should('eq', 'http://localhost:3333/about')
+  it('navigate to map', () => {
+    cy.get('header').contains('Coworkez Maintenant')
+      .click()
+    cy.wait(1000)
+    cy.url().should('eq', 'http://localhost:3333/map')
+  })
 
-  //   cy.get('.shiki')
-  //     .should('exist')
-  // })
+  it('send a message to the team', () => {
+    const xano_suggestion_url = `${import.meta.env.VITE_XANO_API_URL}/api:5e9BgwVw/suggestion`
+
+    cy.intercept('POST', xano_suggestion_url, (req) => {
+    // Assert that the request body includes necessary information
+      expect(req.body).to.include('from_email')
+      expect(req.body).to.include('category')
+      expect(req.body).to.include('body')
+
+      // Respond with a 200 status code without actually sending the email
+      req.reply({
+        statusCode: 200,
+      })
+    })
+    cy.get('input[placeholder=\"Email\"]')
+      .type('bob@bob.bob')
+    cy.get('form').contains('Sujet').click()
+    cy.contains('Question').click()
+    cy.get('textarea[placeholder=\"Ecrivez ici votre petit mot\"]').type('Hello Hello')
+    cy.get('button').contains('Contactez-nous').click()
+    cy.contains('Message envoyé')
+  })
 })
