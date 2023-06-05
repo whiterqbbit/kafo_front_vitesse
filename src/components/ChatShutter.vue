0b80a8edcdf5f354cc180c12cda503ccbc5866c9
sebastien <sebastien.pingal@gmail.com>
2023-06-05 14:22:07 +0200

    fix: front

--- src/components/ChatShutter.vue
+++ src/components/ChatShutter.vue
@@ -2,11 +2,11 @@
   <div class="fixed right-0 top-50px z-40 h-screen--50px max-w-full w-full flex overflow-auto border-cafe-400 bg-cafe-50 p-3 shadow-md sm:w-fit sm:animate-fade-in-right sm:animate-duration-130">
     <!-- liste des chatrooms -->
     <div v-if="conversations" class="flex flex-col gap-5 p-2">
-      <button class="absolute right-0 top-2 z-20 h-7 w-7 rounded-3xl hover:scale-105" icon="pi pi-times" @click="display.chat_shutter = false">
+      <button class="absolute right-0 top-5 z-20 h-7 w-7 rounded-3xl hover:scale-105" icon="pi pi-times" @click="display.chat_shutter = false">
         <img :src="svg_close">
       </button>
-      <div v-if="!preferences.is_mobile || selected_conversation === null" class="text-center text-lg font-bold">
-        Vos conversations
+      <div v-if="!preferences.is_mobile || selected_conversation === null" class="pr-4 text-center text-lg font-bold">
+        Conversations
       </div>
       <div>
         <div
